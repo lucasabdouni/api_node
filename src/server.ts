3a9fb4d26aeff4b114eaca934023a5a6e9c303a2
lucasabdouni <lucasabdouni@hotmail.com>
2023-09-22 13:31:15 -0300

    create test E2E

--- src/server.ts
+++ src/server.ts
@@ -1,27 +1,9 @@
-import fastify from 'fastify'
-import crypto from 'node:crypto'
-import { knex } from './database'
-
-const app = fastify()
-
-app.get('/hello', async () => {
-  // const transaction = await knex('transactions').insert({
-  //   id: crypto.randomUUID(),
-  //   title: 'Transação de teste',
-  //   amount: 1000,
-  // })
-  // .returning('*')
-
-  const transactions = await knex('transactions')
-  .where('amount', 500)
-  .select('*')
-
-  return transactions;
-})
+import { app } from './app'
+import { env } from './env'
 
 app
   .listen({
-    port: 3333,
+    port: env.PORT,
   })
   .then(() => {
     console.log('▫️ HTTP Server Running!')
